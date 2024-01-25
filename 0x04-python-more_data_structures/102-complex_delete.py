3d0038007a942f9919df1a8c49cab27d42cc469c
Astro-Kid <bupechiyana15@gmail.com>
2024-01-25 14:53:14 +0000

    Fine

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+_dictionary, value):
+    list_keys = list(a_dictionary.keys())
+
+    for value_dic in list_keys:
+        if value == a_dictionary.get(value_dic):
+            del a_dictionary[value_dic]
+
+    return (a_dictionary)
