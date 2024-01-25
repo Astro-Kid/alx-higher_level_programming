60f97a3b7f7f3940a018ef8a3251565a6ba87f61
Astro-Kid <bupechiyana15@gmail.com>
2024-01-25 14:49:21 +0000

    yesss

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,5 @@
+t_score(a_dictionary):
+    if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,3 @@
+
+st_map(my_list=[], number=0):
+    return (list(map((lambda i: i * number), my_list)))
